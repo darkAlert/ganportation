0a53413168c71a78f96936c4ea185691236c5815
darkAlert <darkangel-nwo@yandex.ru>
2020-04-03 14:37:55 +0300

    add vibe_rt as submodule

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 import os
 import argparse
 
+import importlib.util
+spec = importlib.util.spec_from_file_location('VibeRT', '/home/darkalert/builds/vibe/lib/models/vibe_rt.py')
+VibeRT = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(VibeRT)
+
 
 
 def main():
@@ -9,6 +14,8 @@ def main():
     parser.add_argument('--root_dir', type=str, default='/home/darkalert/KazendiJob/Data/HoloVideo/Data',
                         help='root dir path')
     args = parser.parse_args()
+
+    vibe = VibeRT(seqlen=16, n_layers=2, hidden_size=1024, add_linear=True, use_residual=True)
 
 if __name__ == '__main__':
     main()
