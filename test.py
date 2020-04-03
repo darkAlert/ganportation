14fd23ca642c0ea747b6f9d1ef51664697e16ef9
darkAlert <darkangel-nwo@yandex.ru>
2020-04-03 18:56:01 +0300

    reinit submodule

--- test.py
+++ test.py
@@ -1,11 +1,8 @@
 import os
 import argparse
-
-import importlib.util
-spec = importlib.util.spec_from_file_location('VibeRT', '/home/darkalert/builds/vibe/lib/models/vibe_rt.py')
-VibeRT = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(VibeRT)
-
+# from thirdparty.vibe_lwgan.models.vibe_rt import VibeRT
+# from thirdparty.vibe_lwgan.vibe_lwgan.lib.models.vibe_rt import VibeRT
+from vibe_lwgan import demo
 
 
 def main():
@@ -15,7 +12,9 @@ def main():
                         help='root dir path')
     args = parser.parse_args()
 
-    vibe = VibeRT(seqlen=16, n_layers=2, hidden_size=1024, add_linear=True, use_residual=True)
+    # vibe = VibeRT(seqlen=16, n_layers=2, hidden_size=1024, add_linear=True, use_residual=True)
+
+    # print (demo)
 
 if __name__ == '__main__':
     main()
