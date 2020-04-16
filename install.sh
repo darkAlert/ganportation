e9e1ef738760f4b20daddaa739a8502b58173d41
darkAlert <darkangel-nwo@yandex.ru>
2020-04-16 18:52:30 +0300

    add setup.py

--- install.sh
+++ install.sh
@@ -1,18 +1,27 @@
 #! /bin/bash
 
 root_dir=~/builds
-name=holoport-cuda101:1.0.0
+img_name=holoport-cuda101:1.0.0
+username=holoanton
 
-mkdir -p $root_dir
-cd $root_dir
+mkdir -p ${root_dir}
+cd ${root_dir}
+
+# get password
+stty -echo
+read -p "Git password please: " password
+stty echo
+printf '\n'
 
 # clone repo
-git clone https://holoanton@bitbucket.org/kazendi/holoportrt.git
-cd $holoportrt
+git config --global credential.helper 'cache --timeout=600'
+git clone https://${username}:${password}@bitbucket.org/kazendi/holoportrt.git holoportrt
+cd holoportrt
+git submodule update --init --recursive
 
 # build
-sudo docker build -t $name .
+docker build -t ${img_name} .
 
 # test
-test_cmd=python3 holoport/tests/test_vibe.py holoport/conf/azure/vibe_conf_azure.yaml
-sudo docker run --gpus all --rm --ipc=host -it $name $test_cmd
+test_cmd='python3 holoport/tests/test_vibe.py holoport/conf/azure/vibe_conf_azure.yaml'
+docker run --gpus all --rm --ipc=host -it ${img_name} ${test_cmd}
---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name='holoport',
+    version='1.0.0',
+    description='Real-Time HoloPortation GAN',
+    author='darkAlert',
+    packages=setuptools.find_packages()
+)
