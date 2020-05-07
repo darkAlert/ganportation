5b61cbb5e55324ddea499b05427f4bfd6f567178
darkAlert <darkangel-nwo@yandex.ru>
2020-05-07 14:36:59 +0300

    remove TrimeshSmplRenderer

--- holoport/smpl_renderer.py
+++ holoport/smpl_renderer.py
@@ -2,36 +2,36 @@ import numpy as np
 import cv2
 import colorsys
 import torch
-from vibert.lib.utils.renderer import Renderer
+#from vibert.lib.utils.renderer import Renderer
 from lwganrt.utils.nmr import SMPLRenderer
 
 
-class TrimeshSmplRenderer():
-    def __init__(self, args):
-        self.mesh_color = colorsys.hsv_to_rgb(np.random.rand(), 0.5, 1.0)
-        self.width = args['width']
-        self.height = args['height']
-
-        self.renderer = Renderer(resolution=(self.width, self.height), orig_img=True,
-                                 wireframe=args['wireframe'], gender=args['gender'],
-                                 smpl_model_dir=args['smpl_model_dir'],
-                                 joint_regressor_path=args['joint_regressor_path'])
-
-    def render(self, verts, cam, resize_to=None):
-        if not isinstance(verts, np.ndarray):
-            scene_verts = verts.numpy()[0]
-        else:
-            scene_verts = verts[0]
-
-        # Render:
-        scene_cam = cam[0]
-        img = np.zeros((self.height,self.width,3), dtype=np.uint8)
-        rendered_smpl = self.renderer.render(img, scene_verts, cam=scene_cam, color=self.mesh_color)
-
-        if resize_to is not None:
-            rendered_smpl = cv2.resize(rendered_smpl, resize_to)
-
-        return rendered_smpl
+# class TrimeshSmplRenderer():
+#     def __init__(self, args):
+#         self.mesh_color = colorsys.hsv_to_rgb(np.random.rand(), 0.5, 1.0)
+#         self.width = args['width']
+#         self.height = args['height']
+#
+#         self.renderer = Renderer(resolution=(self.width, self.height), orig_img=True,
+#                                  wireframe=args['wireframe'], gender=args['gender'],
+#                                  smpl_model_dir=args['smpl_model_dir'],
+#                                  joint_regressor_path=args['joint_regressor_path'])
+#
+#     def render(self, verts, cam, resize_to=None):
+#         if not isinstance(verts, np.ndarray):
+#             scene_verts = verts.numpy()[0]
+#         else:
+#             scene_verts = verts[0]
+#
+#         # Render:
+#         scene_cam = cam[0]
+#         img = np.zeros((self.height,self.width,3), dtype=np.uint8)
+#         rendered_smpl = self.renderer.render(img, scene_verts, cam=scene_cam, color=self.mesh_color)
+#
+#         if resize_to is not None:
+#             rendered_smpl = cv2.resize(rendered_smpl, resize_to)
+#
+#         return rendered_smpl
 
 
 class NeuralSmplRenderer():
