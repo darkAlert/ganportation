52aa65300347e0e39b3325e2e9e39af0c026a843
darkAlert <darkangel-nwo@yandex.ru>
2020-05-20 15:57:41 +0300

    add utils.py

---
+++ holoport/utils.py
@@ -0,0 +1,13 @@
+import cv2
+
+def increase_brightness(img, value=30):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    h, s, v = cv2.split(hsv)
+
+    lim = 255 - value
+    v[v > lim] = 255
+    v[v <= lim] += value
+
+    final_hsv = cv2.merge((h, s, v))
+    img = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
+    return img
